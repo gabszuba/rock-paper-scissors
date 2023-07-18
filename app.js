d5a200f634df349e695e16a33ae306e901c897f5
Gabrielle <118292803+gabszuba@users.noreply.github.com>
2023-07-18 17:28:04 -0300

    Add DOM manipulation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let playerPoints = 0;
 let computerPoints = 0;
+const content = document.querySelector('#content');
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -7,54 +8,34 @@ function getComputerChoice() {
   return choices[random];
 }
 
-function playRound(playerSelection, computerSelection) {  
-  console.log(playerSelection);
-  console.log(computerSelection);
+function playRound(e) {
+  let computerSelection = getComputerChoice().toLowerCase();
+  let playerSelection = e.target.id;
+
+  const result = document.createElement('div');
+
   if (playerSelection === computerSelection) {
-    return "It's a tie";
+    result.textContent = "It's a tie";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    playerPoints++; 
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+    playerPoints++;
+    result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
     computerPoints++;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
   }
+
+  result.classList.add('result');
+  content.appendChild(result);
 }
 
-function getScore() {
-  console.log('Final score:');
-  console.log(`Player: ${playerPoints}`);
-  console.log(`Computer: ${computerPoints}`);
-  if (playerPoints > computerPoints) {
-    return "Congratulations! you won the game. ";
-  } else if (playerPoints < computerPoints) {
-    return "Oh no, computer wins the game... I guess the world domination it's coming";
-  } else {
-    return "We don't have winners";
-  }
-}
-
-const numberOfRounds = 5;
 function game() {
-  for (let i = 0; i < numberOfRounds; i++) {
-    let playerSelection = prompt("Rock Paper or Scissors? ").toLowerCase();
-    let computerSelection = getComputerChoice().toLowerCase();
+  const buttons = Array.from(document.querySelectorAll(".btn"));
 
-    while (!playerSelection || !["rock", "paper", "scissors"].includes(playerSelection)) {
-      console.log("Invalid input");
-      console.log(playerSelection);
-      playerSelection = prompt("Rock, Paper, or Scissors? ").toLowerCase();
-    }
-
-    console.log(playRound(playerSelection, computerSelection));
-
-  }
-  console.log(getScore());
+  buttons.forEach((button) => button.addEventListener("click", playRound));
 }
 
 game();
-
