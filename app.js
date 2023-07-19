b765e7251954b8bcfe41b6fee66c6d8d322c28ee
Gabrielle <118292803+gabszuba@users.noreply.github.com>
2023-07-19 16:48:23 -0300

    Add User Interface

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let playerPoints = 0;
 let computerPoints = 0;
+
 const content = document.querySelector('#content');
+
+const playerScore = document.querySelector('#player-score');
+const computerScore = document.querySelector('#computer-score');
 
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -10,32 +14,76 @@ function getComputerChoice() {
 
 function playRound(e) {
   let computerSelection = getComputerChoice().toLowerCase();
-  let playerSelection = e.target.id;
-
-  const result = document.createElement('div');
+  let playerSelection = e.currentTarget.id;
 
   if (playerSelection === computerSelection) {
-    result.textContent = "It's a tie";
+    return "It's a tie";
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
     playerPoints++;
-    result.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+    return `You won this round! ${playerSelection} beats ${computerSelection}`;
   } else {
     computerPoints++;
-    result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+    return`You lost this round! ${computerSelection} beats ${playerSelection}`;
   }
-
-  result.classList.add('result');
-  content.appendChild(result);
 }
+
+function updateScore() {
+  playerScore.textContent = playerPoints;
+  computerScore.textContent = computerPoints;
+}
+
+const numOfWins = 5;
+function getWinner() {
+  const resultBoard = document.createElement('div');
+  const gameResult = document.createElement('div');
+
+  if (playerPoints >= numOfWins) {
+    gameResult.style.color = "#045b04";
+    gameResult.textContent = "Congratulations! you won the game. ";
+  } else if (computerPoints >= numOfWins) {
+    gameResult.style.color = "#bb0606";
+    gameResult.textContent =  "Oh no, computer won the game... I guess the world domination it's coming";
+  } else return;
+
+  resultBoard.classList.add('result-board');
+
+  gameResult.classList.add('gameResult');
+  resultBoard.appendChild(gameResult);
+  
+  const restartButton = document.createElement('button');
+  restartButton.textContent = 'Restart the Game';
+  restartButton.id = 'restart-button';
+  resultBoard.appendChild(restartButton);
+
+  content.appendChild(resultBoard);
+    
+  restartButton.addEventListener('click', () => { 
+    playerPoints =  computerPoints = 0;
+    content.removeChild(resultBoard);
+    updateScore();
+  })
+} 
 
 function game() {
   const buttons = Array.from(document.querySelectorAll(".btn"));
 
-  buttons.forEach((button) => button.addEventListener("click", playRound));
+  const roundResult = document.createElement('div');
+  roundResult.classList.add('roundResult');
+  content.appendChild(roundResult);
+
+  buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+      roundResult.textContent = playRound(e);
+      console.log("Pontos do jogador:" + playerPoints);
+      console.log("Pontos do computer:" + computerPoints);
+      getWinner();
+      updateScore();
+    });
+  });
 }
 
 game();
